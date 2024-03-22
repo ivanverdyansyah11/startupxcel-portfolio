536099e389c00e27fe0ba65ec0ed8a9e9daf0cf0
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 22:26:01 +0800

    Progress: Add New Component Judges

---
+++ resources/views/partials/judges.blade.php
@@ -0,0 +1,186 @@
+<div class="modal fade" id="modal20" tabindex="-1" aria-labelledby="modal20Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Opening Ceremony and Orientation - 20 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small">The Opening Ceremony and Orientation mark the
+                    official commencement of the Startup Competition, setting the stage for an
+                    exciting journey of innovation and entrepreneurship. This event brings
+                    together participants, mentors, judges, and organizers to create a vibrant
+                    atmosphere of collaboration and learning. During the orientation,
+                    participants will receive an overview of the competition's structure, rules,
+                    and guidelines, ensuring everyone is well-informed and ready to embark on
+                    their entrepreneurial quest. It is a time to inspire and motivate
+                    participants, fostering a sense of camaraderie and determination as they
+                    prepare to showcase their innovative ideas and compete for success. The
+                    Opening Ceremony and Orientation serve as a catalyst for creativity and
+                    ambition, igniting the entrepreneurial spirit and setting the tone for the
+                    transformative experience ahead.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal21" tabindex="-1" aria-labelledby="modal21Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Workshop and Idea Pitch Submission - 21 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small" style="margin-bottom: 8px;">The Workshop and Idea
+                    Pitch Submission session is a
+                    crucial component of the Startup Competition, providing participants with
+                    valuable guidance and an opportunity to showcase their innovative ideas.
+                    This session begins with an engaging workshop led by industry experts, where
+                    participants learn essential skills such as ideation techniques, business
+                    model development, and effective pitching strategies. Through interactive
+                    discussions and practical exercises, participants gain insights on refining
+                    their ideas and shaping them into compelling business propositions.</p>
+                <p class="paragraph-small">Following the workshop, participants have the chance
+                    to submit their idea pitches, putting into practice what they have learned.
+                    This submission serves as a platform for participants to present their ideas
+                    in a concise and impactful manner, capturing the attention of judges and
+                    potential investors. Participants are encouraged to highlight the unique
+                    value proposition of their startup, its market potential, and the problem it
+                    aims to solve.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal22" tabindex="-1" aria-labelledby="modal22Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Idea Evaluation and Mentorship - 22 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small" style="margin-bottom: 8px;">The Idea Evaluation and
+                    Mentorship phase is a pivotal stage of the Startup Competition, where
+                    participants receive valuable feedback and guidance from experienced
+                    mentors. During this phase, each participant's idea is carefully evaluated
+                    by a panel of experts who assess its feasibility, market potential, and
+                    scalability. Participants receive constructive feedback on various aspects
+                    of their idea, helping them refine and strengthen their business concepts.
+                </p>
+                <p class="paragraph-small">In addition to idea evaluation, participants are
+                    paired with mentors who have industry-specific expertise and a wealth of
+                    entrepreneurial knowledge. These mentors provide one-on-one guidance,
+                    sharing insights, best practices, and actionable advice to help participants
+                    navigate the challenges of building a successful startup. Through mentorship
+                    sessions, participants gain invaluable support in developing their business
+                    strategies, refining their value propositions, and enhancing their overall
+                    entrepreneurial skills.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal23" tabindex="-1" aria-labelledby="modal23Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Business Plan Development and Semi-Finals - 23 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small" style="margin-bottom: 8px;">The Business Plan
+                    Development and Semi-Finals phase is a critical stage in the Startup
+                    Competition, where participants refine their business plans and compete for
+                    a spot in the final round. During this phase, participants work diligently
+                    to develop comprehensive business plans that outline their value
+                    proposition, target market, financial projections, and growth strategies.
+                    They receive expert guidance and support from mentors and advisors, helping
+                    them fine-tune their plans and ensure their viability in the market.
+                </p>
+                <p class="paragraph-small">Once the business plans are finalized, participants
+                    present their pitches in the semi-finals. This is a pivotal moment where
+                    they showcase their innovative ideas, market potential, and execution plans
+                    to a panel of judges and potential investors. The semi-finals provide
+                    participants with a platform to demonstrate their entrepreneurial acumen,
+                    compelling storytelling, and ability to communicate their vision
+                    effectively.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal24" tabindex="-1" aria-labelledby="modal24Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Semi-Finals and Pitch Training - 24 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small" style="margin-bottom: 8px;">The Semi-Finals and
+                    Pitch Training phase is a crucial stage in the Startup Competition, where
+                    participants refine their pitches and compete for a spot in the final round.
+                    During this phase, participants undergo intensive pitch training sessions
+                    facilitated by experienced mentors and coaches. These sessions focus on
+                    enhancing participants' presentation skills, refining their storytelling
+                    techniques, and fine-tuning their pitch delivery to captivate the judges and
+                    investors.
+                </p>
+                <p class="paragraph-small">Participants receive valuable feedback and guidance
+                    on their pitch content, structure, and overall presentation style. They
+                    learn how to effectively communicate their unique value proposition, market
+                    potential, and growth strategies. Through practice sessions and mock
+                    pitches, participants gain confidence in delivering compelling and
+                    persuasive presentations that highlight the strengths of their startup.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal25" tabindex="-1" aria-labelledby="modal25Label" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h6>Finals and Awards Ceremony - 25 January 2023</h6>
+            </div>
+            <div class="modal-body">
+                <p class="paragraph-small" style="margin-bottom: 8px;">The Semi-Finals and
+                    Pitch Training phase is a crucial stage in the Startup Competition, where
+                    participants refine their pitches and compete for a spot in the final round.
+                    During this phase, participants undergo intensive pitch training sessions
+                    facilitated by experienced mentors and coaches. These sessions focus on
+                    enhancing participants' presentation skills, refining their storytelling
+                    techniques, and fine-tuning their pitch delivery to captivate the judges and
+                    investors.
+                </p>
+                <p class="paragraph-small">Participants receive valuable feedback and guidance
+                    on their pitch content, structure, and overall presentation style. They
+                    learn how to effectively communicate their unique value proposition, market
+                    potential, and growth strategies. Through practice sessions and mock
+                    pitches, participants gain confidence in delivering compelling and
+                    persuasive presentations that highlight the strengths of their startup.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="button-light" data-bs-dismiss="modal">Close
+                    Modal</button>
+            </div>
+        </div>
+    </div>
+</div>
