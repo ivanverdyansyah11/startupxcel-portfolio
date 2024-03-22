232898244b78cc0f839967d3ec79953e95ce1cf2
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-03-22 22:25:43 +0800

    Progress: Add New Component Footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,65 @@
+{{-- FOOTER --}}
+<footer class="footer" id="footer">
+    <div class="container">
+        <div class="row" style="margin-bottom: 100px">
+            <div class="col-6 col-md-3 mb-5 mb-md-0">
+                <a class="footer-brand d-inline-block" href="#home" style="margin-bottom: 18px;">
+                    <img src="{{ asset('assets/img/logo/logo-brand.svg') }}" class="img-fluid w-100" alt="Logo Brand">
+                </a>
+                <p class="paragraph-small" style="margin-bottom: 30px;">Welcome to StartUpXcel, a platform
+                    designed to ignite and empower startups from around the world.</p>
+                <div class="wrapper-sosmed d-flex gap-2">
+                    <a href="https://id-id.facebook.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/facebook-icon.svg') }}" class="img-fluid"
+                            alt="Facebook Icon" width="15">
+                    </a>
+                    <a href="https://www.whatsapp.com/" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/whatsapp-icon.svg') }}" class="img-fluid"
+                            alt="Whatsapp Icon" width="15">
+                    </a>
+                    <a href="https://twitter.com/?lang=id" target="_blank"
+                        class="sosmed-icon d-flex justify-content-center align-items-center">
+                        <img src="{{ asset('assets/img/icon/twitter-icon.svg') }}" class="img-fluid" alt="Twitter Icon"
+                            width="15">
+                    </a>
+                </div>
+            </div>
+
+            <div class="offset-lg-2 offset-xl-3 col-6 col-md-3 col-lg-2 mb-5 mb-md-0">
+                <h6 style="margin-bottom: 20px">Quick Links</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#home" class="paragraph-small">Home</a>
+                    <a href="#about" class="paragraph-small">About Us</a>
+                    <a href="#schedule" class="paragraph-small">Schedule Competition</a>
+                    <a href="#judging" class="paragraph-small">Judging Panel</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-xl-2">
+                <h6 style="margin-bottom: 20px">Rule Competition</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#rule" class="paragraph-small">Eligibility</a>
+                    <a href="#rule" class="paragraph-small">Submission Guidelines</a>
+                    <a href="#rule" class="paragraph-small">Evaluation Process</a>
+                    <a href="#rule" class="paragraph-small">Intellectual Property</a>
+                </div>
+            </div>
+            <div class="col-6 col-md-3 col-lg-2">
+                <h6 style="margin-bottom: 20px">Help & guide</h6>
+                <div class="wrapper d-flex flex-column gap-3">
+                    <a href="#" class="paragraph-small">Terms & Conditions</a>
+                    <a href="#" class="paragraph-small">Privacy Policy</a>
+                    <a href="#" class="paragraph-small">Contact Us</a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-12 copyright text-center">
+                <p>Copyright © 2023 StartUpXcel. All Right Reserved.</p>
+            </div>
+        </div>
+    </div>
+</footer>
+{{-- END FOOTER --}}
